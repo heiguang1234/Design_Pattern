9185c81ab5c42cce10878a2d80c590012a810fa2
heiguang1234 <1814542535@qq.com>
2024-07-02 20:16:56 +0800

    homework01

--- src/main/java/com/cl/syn/Homework/Homework02.java
+++ src/main/java/com/cl/syn/Homework/Homework02.java
@@ -1,21 +1,38 @@
 package com.cl.syn.Homework;
 
+import com.sun.org.apache.xerces.internal.impl.dtd.models.CMLeaf;
+import org.junit.internal.runners.statements.RunAfters;
+
 public class Homework02 {
-}
+    public static void main(String[] args) {
+        Client01 client01 = new Client01();
+        new Thread(client01).start();
+        new Thread(client01).start();
 
 
-class Client01 extends Thread{
-    private  static int CreditSum=10000;
-    @Override
-    public synchronized void run() {
-        CreditSum-=1000;
-        System.out.println(this.getName()+"取款一次，剩余余额为："+CreditSum);
     }
 }
 
-class Client02 extends Thread{
+
+class Client01 implements Runnable {
+    private int CreditSum = 10000;
+    private static void  m1(){
+        synchronized (Client01.class){
+            System.out.println("m1");
+        }
+    }
+
     @Override
     public void run() {
-        super.run();
+        synchronized (this) {
+            while (true) {
+                if (CreditSum <= 0) {
+                    System.out.println("余额不足");
+                    break;
+                }
+                CreditSum-=1000;
+                System.out.println(Thread.currentThread().getName()+"取款一次，剩余："+CreditSum);
+            }
+        }
     }
 }
